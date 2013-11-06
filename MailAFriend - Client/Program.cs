595961b52b45e6a04eebda72661f3138a6d6673b
MattWPhillips <mantis_rulze@hotmail.com>
2013-11-06 23:34:09 +1100

    version 0.05

--- MailAFriend - Client/Program.cs
+++ MailAFriend - Client/Program.cs
@@ -16,7 +16,7 @@ namespace MailAFriend___Client
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormClient());
         }
     }
 }
